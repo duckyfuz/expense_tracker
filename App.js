bcc3aeb9185828d156ca2313125240d362d40b75
duckyfuz <kenf@iMac&cheese.local>
2023-06-18 17:02:39 +0800

    Add paper bottomTab

--- App.js
+++ App.js
@@ -1,37 +1,42 @@
 import * as React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, StyleSheet, SafeAreaView } from "react-native";
+import { Text, BottomNavigation } from "react-native-paper";
 import { StatusBar } from "expo-status-bar";
 
-import { NavigationContainer } from "@react-navigation/native";
+import {
+  NavigationContainer,
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from "@react-navigation/native";
+import {
+  MD3DarkTheme,
+  MD3LightTheme,
+  PaperProvider,
+  adaptNavigationTheme,
+} from "react-native-paper";
+import merge from "deepmerge";
+
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { Ionicons } from "@expo/vector-icons";
+// import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+// import IconButton from "./components/UI/IconButton";
+// import { Ionicons } from "@expo/vector-icons";
 
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-
-import { GlobalStyles } from "./constants/styles";
+import ExpensesContextProvider from "./store/expenses-context";
+import * as Notifications from "expo-notifications";
 
 import AllExpenses from "./screens/AllExpenses";
 import ManageExpense from "./screens/ManageExpense";
 import RecentExpenses from "./screens/RecentExpenses";
-import ExpensesContextProvider from "./store/expenses-context";
 
-import * as Notifications from "expo-notifications";
-import IconButton from "./components/UI/IconButton";
+import { GlobalStyles } from "./constants/styles";
 
-import {
-  MD3DarkTheme as DefaultTheme,
-  MD3LightTheme as LightTheme,
-  PaperProvider,
-} from "react-native-paper";
+const { LightTheme, DarkTheme } = adaptNavigationTheme({
+  reactNavigationLight: NavigationDefaultTheme,
+  reactNavigationDark: NavigationDarkTheme,
+});
 
-const theme = {
-  ...DefaultTheme,
-  colors: {
-    ...DefaultTheme.colors,
-    primary: "tomato",
-    secondary: "yellow",
-  },
-};
+const CombinedDefaultTheme = merge(MD3LightTheme, LightTheme);
+const CombinedDarkTheme = merge(MD3DarkTheme, DarkTheme);
 
 Notifications.setNotificationHandler({
   handleNotification: async () => ({
@@ -42,93 +47,145 @@ Notifications.setNotificationHandler({
 });
 
 const Stack = createNativeStackNavigator();
-const Tab = createBottomTabNavigator();
+// const Tab = createBottomTabNavigator();
 
 function NullComponent() {
-  <Text>This should never show up</Text>;
+  return (
+    <SafeAreaView style={{ flex: 1 }}>
+      <View style={{ justifyContent: "center", alignItems: "center", flex: 1 }}>
+        <Text variant="displayMedium" style={{ textAlign: "center" }}>
+          This page is being worked on! Check again in a week :)
+        </Text>
+      </View>
+    </SafeAreaView>
+  );
 }
 
 const ExpensesOverview = () => {
+  const [index, setIndex] = React.useState(0);
+  const [routes] = React.useState([
+    {
+      key: "expenses",
+      title: "Expenses",
+      focusedIcon: "cash-multiple",
+      unfocusedIcon: "cash-fast",
+    },
+    {
+      key: "report",
+      title: "Report",
+      focusedIcon: "calendar-month",
+      unfocusedIcon: "calendar-month-outline",
+    },
+    {
+      key: "budgets",
+      title: "Budgets",
+      focusedIcon: "bag-personal",
+      unfocusedIcon: "bag-personal-outline",
+    },
+    {
+      key: "settings",
+      title: "Settings",
+      focusedIcon: "account-settings",
+      unfocusedIcon: "account-settings-outline",
+    },
+  ]);
+
+  const renderScene = BottomNavigation.SceneMap({
+    expenses: RecentExpenses,
+    report: AllExpenses,
+    budgets: NullComponent,
+    settings: NullComponent,
+  });
+
   return (
-    <Tab.Navigator
-      screenOptions={{
-        tabBarShowLabel: false,
-        headerStyle: { backgroundColor: GlobalStyles.colors.background },
-        headerTintColor: GlobalStyles.colors.onBackground,
-        tabBarStyle: {
-          ...styles.tabBar,
-          ...styles.shadow,
-        },
-        tabBarActiveTintColor: GlobalStyles.colors.secondary,
-        headerShadowVisible: false,
-      }}
-    >
-      <Tab.Screen
-        name="RecentExpenses"
-        component={RecentExpenses}
-        options={{
-          headerRight: () => (
-            <IconButton
-              icon="save"
-              size={24}
-              color={GlobalStyles.colors.secondary}
-              onPress={scheduleNotificationHandler}
-              additionalStyle={{ marginRight: 12 }}
-            />
-          ),
-          title: "Recent Expenses",
-          tabBarLabel: "Recent",
-          tabBarIcon: ({ color, size }) => (
-            <View style={styles.tabBarIcon}>
-              <Ionicons name="hourglass" size={size} color={color} />
-            </View>
-          ),
-        }}
-      />
-      <Tab.Screen
-        name="Add"
-        component={NullComponent}
-        options={{
-          tabBarIcon: ({ focused, size, color }) => (
-            <View style={styles.tabBarIconCenter}>
-              <Ionicons
-                name="add-circle"
-                size={100}
-                color={GlobalStyles.colors.primaryVariant}
-              />
-            </View>
-          ),
-        }}
-        listeners={({ navigation }) => ({
-          tabPress: (e) => {
-            e.preventDefault();
-            navigation.navigate("ManageExpense");
-          },
-        })}
-      />
-      <Tab.Screen
-        name="AllExpenses"
-        component={AllExpenses}
-        options={{
-          title: "All Expenses",
-          tabBarLabel: "All Expenses",
-          tabBarIcon: ({ color, size }) => (
-            <View style={[styles.tabBarIcon, styles.shadow]}>
-              <Ionicons name="calendar" size={size} color={color} />
-            </View>
-          ),
-        }}
-      />
-    </Tab.Navigator>
+    <BottomNavigation
+      navigationState={{ index, routes }}
+      onIndexChange={setIndex}
+      renderScene={renderScene}
+    />
   );
+
+  // Previous implementation - floating bottomTab with large center button
+  // return (
+  //   <Tab.Navigator
+  //     screenOptions={{
+  //       tabBarShowLabel: false,
+  //       headerStyle: { backgroundColor: GlobalStyles.colors.background },
+  //       headerTintColor: GlobalStyles.colors.onBackground,
+  //       tabBarStyle: {
+  //         ...styles.tabBar,
+  //         ...styles.shadow,
+  //       },
+  //       tabBarActiveTintColor: GlobalStyles.colors.secondary,
+  //       headerShadowVisible: false,
+  //     }}
+  //   >
+  //     <Tab.Screen
+  //       name="RecentExpenses"
+  //       component={RecentExpenses}
+  //       options={{
+  //         headerRight: () => (
+  //           <IconButton
+  //             icon="save"
+  //             size={24}
+  //             color={GlobalStyles.colors.secondary}
+  //             onPress={scheduleNotificationHandler}
+  //             additionalStyle={{ marginRight: 12 }}
+  //           />
+  //         ),
+  //         title: "Recent Expenses",
+  //         tabBarLabel: "Recent",
+  //         tabBarIcon: ({ color, size }) => (
+  //           <View style={styles.tabBarIcon}>
+  //             <Ionicons name="hourglass" size={size} color={color} />
+  //           </View>
+  //         ),
+  //       }}
+  //     />
+  //     <Tab.Screen
+  //       name="Add"
+  //       component={NullComponent}
+  //       options={{
+  //         tabBarIcon: ({ focused, size, color }) => (
+  //           <View style={styles.tabBarIconCenter}>
+  //             <Ionicons
+  //               name="add-circle"
+  //               size={100}
+  //               color={GlobalStyles.colors.primaryVariant}
+  //             />
+  //           </View>
+  //         ),
+  //       }}
+  //       listeners={({ navigation }) => ({
+  //         tabPress: (e) => {
+  //           e.preventDefault();
+  //           navigation.navigate("ManageExpense");
+  //         },
+  //       })}
+  //     />
+  //     <Tab.Screen
+  //       name="AllExpenses"
+  //       component={AllExpenses}
+  //       options={{
+  //         title: "All Expenses",
+  //         tabBarLabel: "All Expenses",
+  //         tabBarIcon: ({ color, size }) => (
+  //           <View style={[styles.tabBarIcon, styles.shadow]}>
+  //             <Ionicons name="calendar" size={size} color={color} />
+  //           </View>
+  //         ),
+  //       }}
+  //     />
+  //   </Tab.Navigator>
+  // );
 };
 
 export default function App() {
   return (
-    <PaperProvider theme={theme}>
+    <PaperProvider theme={CombinedDarkTheme}>
       <ExpensesContextProvider>
         <StatusBar style="light" />
-        <NavigationContainer>
+        <NavigationContainer theme={CombinedDarkTheme}>
           <Stack.Navigator
             screenOptions={{
               tabBarShowLabel: false,
