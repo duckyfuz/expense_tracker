b0830a2322c2558663fb3ad5bc5aad16b9f87eaa
duckyfuz <kenf@iMac&cheese.local>
2023-06-21 16:01:46 +0800

    Clean up ExpenseItem

--- components/ExpensesOutput/ExpenseItem.js
+++ components/ExpensesOutput/ExpenseItem.js
@@ -2,30 +2,42 @@ import { Pressable, View, StyleSheet } from "react-native";
 import { Card, Text } from "react-native-paper";
 import { useNavigation } from "@react-navigation/native";
 
-import { GlobalStyles } from "../../constants/styles";
 import { getFormattedDate } from "../../util/date";
 
 const ExpenseItem = ({ id, description, amount, date }) => {
   const navigation = useNavigation();
 
   function expensePressHandler() {
+    // Can try implementing a zoom out (for the background) when opening the modal
     navigation.navigate("ManageExpense", { expenseId: id });
   }
+
+  const TitleDate = () => {
+    return (
+      <View>
+        <Text variant="titleLarge">{description}</Text>
+        <Text variant="labelMedium">{getFormattedDate(date)}</Text>
+      </View>
+    );
+  };
+
+  const AmountContainer = () => {
+    return (
+      <Card mode="outlined" style={styles.amountContainer}>
+        <Text style={{ fontWeight: "bold" }}>${amount.toFixed(2)}</Text>
+      </Card>
+    );
+  };
 
   return (
     <Pressable
       onPress={expensePressHandler}
       style={({ pressed }) => pressed && styles.pressed}
     >
-      <Card style={{ marginVertical: 2 }}>
+      <Card style={{ margin: 2 }}>
         <View style={styles.expenseItem}>
-          <View>
-            <Text variant="titleLarge">{description}</Text>
-            <Text variant="labelMedium">{getFormattedDate(date)}</Text>
-          </View>
-          <Card mode="outlined" style={styles.amountContainer}>
-            <Text style={styles.amount}>${amount.toFixed(2)}</Text>
-          </Card>
+          <TitleDate />
+          <AmountContainer />
         </View>
       </Card>
     </Pressable>
@@ -39,18 +51,15 @@ const styles = StyleSheet.create({
     opacity: 0.75,
   },
   expenseItem: {
-    padding: 6,
+    marginVertical: 6,
+    marginHorizontal: 10,
     flexDirection: "row",
     justifyContent: "space-between",
-    marginHorizontal: 5,
   },
   amountContainer: {
     alignItems: "center",
     justifyContent: "center",
     minWidth: 80,
     height: "100%",
-  },
-  amount: {
-    fontWeight: "bold",
   },
 });
